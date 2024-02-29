dbdcb806678dfc15a42eda45179cfc4d7a28a1dc
Thanawatben <jitrada5964@gmail.com>
2024-02-29 15:31:02 +0700

    Loginpage,Registerpage ui

---
+++ src/pages/User/RegisterPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Input from '../../components/Input'
+
+function RegisterPage() {
+  return (
+    <div className='max-w-[1024] w-8/12 mx-auto flex flex-col items-center bg-gray_primary'>
+        <form className=' w-8/12 bg-white h-full  my-12 flex flex-col items-center'>
+            <div className=' mt-2 mb-6  flex flex-col items-center  w-2/4 gap-5 '>
+                <h1 className='text-xl font-bold ite'>สมัครสมาชิกวงใน</h1>
+                <div className=' w-28 h-28'><img className=' rounded-full' src='https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png'/></div>
+                <Input placeholder='เบอร์โทร หรือ อีเมลล์' />
+                <Input placeholder='ชื่อ' />
+                <Input placeholder='พาสเวิร์ด' />
+                <Input placeholder='ยืนยันพาสเวิร์ด' />
+                <Input placeholder='วันเกิด' />
+                <div className=' w-full'>
+                    <p className='text-gray-400'>เพศ</p>
+                    <div className='w-full flex justify-around'>
+                 <button className='border-2 w-20 text-gray-500 font-bold rounded-lg px-3 py-1 m-0 '>ชาย</button>
+                 <button className='border-2 w-20 text-gray-500 font-bold rounded-lg px-3 py-1 m-0 '>หญิง</button>
+                 <button className='border-2 w-20 text-gray-500 font-bold rounded-lg px-3 py-1 m-0  '>ไม่ระบุ</button>
+                </div>
+                </div>
+                
+                <button className='text-white w-full rounded-lg px-3 py-2 m-0 bg-blue_primary'>ถัดไป</button>
+                
+                 
+                
+            </div>
+            
+        </form>
+    </div>
+  )
+}
+
+export default RegisterPage
--- src/route/index.jsx
+++ src/route/index.jsx
@@ -4,7 +4,8 @@ import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 
 import Header from '../layouts/Header';
 import UserReview from '../pages/User/UserReview';
-
+import LoginPage from '../pages/User/LoginPage';
+import RegisterPage from '../pages/User/RegisterPage';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -15,7 +16,10 @@ const router = createBrowserRouter([
       </div>
     ),
     children: [
-      { path: '/review',element: <UserReview/>}],
+      { path: '/review',element: <UserReview/>},
+      { path: '/login', element: <LoginPage/>},
+      { path: '/register', element: <RegisterPage/>}],
+      
   },
 ]);
 
