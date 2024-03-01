23c7089a130af44f486e98d3698115a7605d7a0c
Firstkris <breezephyrfirst@gmail.com>
2024-03-01 15:27:07 +0700

    update localstorage.js

--- src/utils/local-storage.js
+++ src/utils/local-storage.js
@@ -1,1 +1,5 @@
-export const getToken = () => localStorage.getItem("token");
+const ACCESS_TOKEN = "ACCESS_TOKEN"
+
+export const setToken = (token) => localStorage.setItem(ACCESS_TOKEN, token);
+export const getToken = () => localStorage.getItem(ACCESS_TOKEN);
+export const clearToken = () => localStorage.removeItem(ACCESS_TOKEN);
