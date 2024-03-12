c9ea9dcdafc34fc9a3594be9cf0bb8a3b053ecc0
warakornC <pleumbook@gmail.com>
2024-03-12 17:27:10 +0700

    reloading login

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -125,7 +125,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/merchant/editRestaurant/:merchantId/:restaurantId",
-        element: <EditBusinessInfoPage />,
+        element: 
+        <MerchantContextProvider>
+
+        <EditBusinessInfoPage />,
+        </MerchantContextProvider>
       },
       {
         path: "/merchant/:merchantId/:restaurantId",
